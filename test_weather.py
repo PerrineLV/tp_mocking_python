b743989b31685b7ab600096032a09fe7d21fc64e
Perrine Launay <plaunay@medinplus.com>
2025-06-20 10:26:54 +0200

    typos

--- test_weather.py
+++ test_weather.py
@@ -7,11 +7,11 @@ class TestWeather(unittest.TestCase):
     def test_get_temperature_paris(self, mock_get):
         """Premierest avec mock"""
 
-        #1. CRÉEZ un objet Mock pour simuler la réponse HTTP
+        # 1. CRÉEZ un objet Mock pour simuler la réponse HTTP
         fake_response = Mock()
         fake_response.status_code = 200
 
-        #2. CRÉEZ les données JSON que l'API retournerait
+        # 2. CRÉEZ les données JSON que l'API retournerait
         fake_response.json.return_value = {
             'main': {
                 'temp': 25.5
@@ -21,16 +21,15 @@ class TestWeather(unittest.TestCase):
         # 3. CONFIGUREZ le mock pour retourner votre fake_response
         mock_get.return_value = fake_response
 
-        #4. TESTEZ votre fonction
+        # 4. TESTEZ votre fonction
         result = get_temperature('Paris')
 
-        #5. VERIFIEZ le résultat
+        # 5. VERIFIEZ le résultat
         self.assertEqual(result, 25.5)
 
         # 6. VÉRIFIEZ que requests.get a été appelé correctement
         mock_get.assert_called_once_with('http://api.openweathermap.org/data/2.5/weather',
-                                           params={'q': 'Paris', 'appid': 'fake_api_key', 'units': 'metric'})  
-        
+                                           params={'q': 'Paris', 'appid': 'fake_api_key', 'units': 'metric'})
 
     @patch('weather_service.requests.get')
     def test_get_temperature_city_not_found(self, mock_get):
